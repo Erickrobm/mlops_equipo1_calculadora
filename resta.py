cc945d925bb417b62e37a637e64867d0f55ddb76
Carlos Andrade <carlos.andrade@volaris.com>
2022-03-12 12:24:20 -0600

    Adicion de la funcionalidad resta, toma dos numeros flotantes y regresa la resta de entre el primer argumento menos el segundo argumento

---
+++ resta.py
@@ -0,0 +1,19 @@
+def resta(a,b):
+    """
+
+    Parameters
+    ----------
+    a : int, float, string.
+    Primer valor definido para la operación.
+        
+    b : int, float, string.
+    Segundo valor definifo para la operación.  
+
+    Returns
+    -------
+    Valor obtenido por la resta de los parámetros.
+
+    """
+    minuendo = float(a)
+    sustraendo = float(b)
+    return minuendo - sustraendo
