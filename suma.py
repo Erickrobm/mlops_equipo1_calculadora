008459ccab9e1847fb124c523ef225ae89ccf06f
Erick Robledo <eriickeduardo97@gmail.com>
2022-03-12 12:15:50 -0600

    Actualizacion a la funcion suma, se agrego la seccion de comentario

--- suma.py
+++ suma.py
@@ -1,4 +1,15 @@
-def suma(a, b):
-  a = get_fractions(a)
-  b = get_fractions(b)
-  return a + b
+def suma(sumando_a, sumando_b):
+  """
+  Regresa el valor tipo float de una suma de dos variables
+    Parameters
+    ----------
+        valor: str :
+            El valor que se va a convertir a float
+    Returns
+    -------
+        returns:
+            El valor en float
+"""
+  sumando_a = get_fractions(sumando_a)
+  sumando_b = get_fractions(sumando_b)
+  return sumando_a + sumando_b
