f3395cdbd76dabf4370a6c68e6fdf01a2ef956a8
Francisco Ibarra <silentium77@hotmail.com>
2022-03-12 13:33:20 -0600

    Este es el codigo inicial del proyecto

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Proyecto de calculadora realizado en mlops con suma,resta, division y multiplicacion',
+    author='Francisco Ibarra',
+    license='',
+)
